1936abd5bcb6c6ac505dcc769a11086cda4584dc
Nicolas Stirnemann <nstirnemann59@gmail.com>
2024-08-22 23:34:28 -0300

    Purple Screen

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
 def main():
     print("Starting asteroids!")
+    print("Screen width: ", SCREEN_WIDTH)
+    print("Screen height: ", SCREEN_HEIGHT)
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("purple")
+
+        pygame.display.flip()
+
+        clock.tick(60)
+
+    pygame.quit()
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     main()
